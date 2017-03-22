779278a25b8d42255e286ebc150ed5eceaf58fbf
Makuschenko Sergey <me@example.com>
2017-03-22 22:51:25 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+	/*	function menu (x) {
+			// body...
+			alert("I love" + x);
+		}
+	menu("borsch");
+	menu("grechka");
+	menu("kompot");*/
+	
+
+	//next lesson 8
+	/*function choise(one, two){
+		document.write(one + " better when " + two + "<br />");
+	}
+	choise("borsch", "soup");
+	choise("summer", "winter ");
+	choise("putin", "poroshenko");*/
+
+	//lesson 9
