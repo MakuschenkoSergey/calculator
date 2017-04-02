025c1e1446f29b1b45d794ee130bde1136246de0
Makuschenko Sergey <me@example.com>
2017-04-02 20:17:32 +0300

    it worksgit status!

--- libs/custom/main.js
+++ libs/custom/main.js
@@ -1,18 +1,42 @@
-	/*	function menu (x) {
-			// body...
-			alert("I love" + x);
-		}
-	menu("borsch");
-	menu("grechka");
-	menu("kompot");*/
-	
 
-	//next lesson 8
-	/*function choise(one, two){
-		document.write(one + " better when " + two + "<br />");
-	}
-	choise("borsch", "soup");
-	choise("summer", "winter ");
-	choise("putin", "poroshenko");*/
+'use strict';
 
-	//lesson 9
+// console.log($.fn.jquery);
+// console.log('ok');
+
+$(document).ready(function (){
+
+  var price = {
+    bond : 19.9,
+		winston : 25,
+		lm : 21,
+		camel : 26,
+		marlboro : 28,
+		parlament : 35
+	};
+
+  $("#mark").change(function () {
+		var mark = $('#mark').val();
+		console.log(mark);
+  });
+
+  var quantity = 0;
+  $("#quantity").change(function () {
+    quantity = $("#quantity").val();
+    console.log(quantity);
+  });
+
+  var period = {
+  	week : 7,
+		month: 30,
+		season : 90,
+		year : 360
+	};
+
+	var result = 0;
+  $("#btn").click(function (){
+		result = price[mark.value] * (quantity / 20) * 7;
+		$("#result").val(Math.round((result*100)/100));
+  });
+
+});
